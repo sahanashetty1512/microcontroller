9baca6699636a6ac4eb4ce27e77dc575160c5d28
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2025-03-09 12:07:20 +0530

    Add files via upload

---
+++ prg.asm
@@ -0,0 +1,37 @@
+	 AREA PRG,CODE,READONLY
+START
+         MOV R0,#0
+         MOV R1,#10
+	 
+         ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 BX LR
+END
